286110b55ceb8c07591cf7a1823b98348c42ff57
Icaro Siqueira <icarorob@hotmail.com>
2025-02-13 16:46:41 -0300

    Improves list, loading and error message styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { ApolloClient, InMemoryCache, useQuery, gql, ApolloProvider } from '@apollo/client';
 import { styled } from 'styled-components'
+import loading_semicircle from './loading-icon.svg';
 
 
 function App() {
@@ -8,15 +9,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>
-        <Wrapper>
-          <Title>
-            Country Catalog
-          </Title>
-        </Wrapper>
-          <br/>
+          <Wrapper>
           <ApolloProvider client={client}>
-            <DisplayLocations />
-          </ApolloProvider>,
+            <PageTitle/>
+            <br/>
+              <CountryTable>
+                <DisplayTableHeaders/>
+                <DisplayLocations/>
+              </CountryTable>
+            </ApolloProvider>
+          </Wrapper>
         </div>
       </header>
     </div>
@@ -28,22 +30,53 @@ export default App;
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  color:rgb(255, 255, 255);
+  padding: 2em;
+  border-radius: 15px;
+  background:  rgb(186, 228, 252);
 `;
-
-const CountryList = styled.ul`
-  text-align: left;
-  border-radius: 25px;
-  border: 2px solid rgb(255, 255, 255);
-  padding: 20px;
-  margin: 0px;
-`
 
 const Wrapper = styled.section`
-  padding: 2em;
-  border-radius: 25px;
-  border: 2px solid rgb(255, 255, 255);
 `;
+
+const CountryTable = styled.table`
+  overflow: hidden;
+  box-shadow: 0 0 0 1px rgb(219, 218, 218);
+  border-radius: 15px;
+  border-collapse: collapse;
+`;
+
+const TableRow = styled.tr`
+  ${props =>
+    props.even &&
+    `
+        background: rgb(219, 218, 218);
+    `}
+`;
+
+const TableHeader = styled.th`
+  background: rgb(219, 218, 218);
+  padding: 0.5%;
+  border-bottom: medium solid black;
+`;
+
+const TableItem = styled.td`
+  padding: 0.125rem 0.75rem;
+`;
+
+const LoadingMessage = styled.div`
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+const ErrorMessage = styled.h3`
+    position: fixed;
+    left: 50%;
+    top: 40%;
+    transform: translate(-50%, -50%);
+    color: red;
+`
 
 const client = new ApolloClient({
   uri: 'https://countries.trevorblades.com/graphql',
@@ -63,15 +96,56 @@ const GET_LOCATIONS = gql`
   }
 `;
 
+function DisplayTableHeaders() {
+  const { loading, error } = useQuery(GET_LOCATIONS);
+
+  if (loading || error) {
+    return;
+  }
+
+  return <TableRow>
+    <TableHeader>Flag</TableHeader>
+    <TableHeader>Name</TableHeader>
+    <TableHeader>Capital</TableHeader>
+    <TableHeader>Language(s)</TableHeader>
+  </TableRow>
+}
+
+
 function DisplayLocations() {
   const { loading, error, data } = useQuery(GET_LOCATIONS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
+  if (loading) return LoadingPage();
+  if (error) return ErrorPage(error);
 
-  return data.countries.map(({ emoji, name, capital, languages }) => (
-    <div>
-      <CountryList>{emoji} {name}, capital: {capital}, languages: {languages.map((language) => language.name).join(', ')}</CountryList>
-    </div>
+  return data.countries.map(({ emoji, name, capital, languages }, index) => (
+    <TableRow even={(index + 1)% 2 === 0}>
+      <TableItem>{emoji}</TableItem>
+      <TableItem>{name}</TableItem>
+      <TableItem>{capital || 'N/A'}</TableItem>
+      <TableItem>{languages.map((language) => language.name).join(', ') || 'N/A'}</TableItem>
+    </TableRow>
   ));
+}
+
+function LoadingPage() {
+  return <LoadingMessage>
+    <img src={loading_semicircle} className="Loading-icon" alt="logo" width="200" height="200"/>
+    <p>Loading...</p>
+  </LoadingMessage>
+}
+
+function ErrorPage(error) {
+  return <ErrorMessage>Error : {error.message}</ErrorMessage>
+}
+
+
+function PageTitle() {
+  const { loading, error } = useQuery(GET_LOCATIONS);
+
+  if (loading || error) {
+    return;
+  }
+
+  return <Title>Country Catalog</Title>
 }
